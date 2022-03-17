c35cc0274256093d0682276325fc6251a73712df
koAibo <97710267+koAibo@users.noreply.github.com>
2022-03-17 16:38:35 -0400

    Initial commit

---
+++ app/src/main/java/com/cst2335/teamproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cst2335.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Kevin Ong, Vincent Zheng, Minh Trung Do
+ * @version 1.0
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
