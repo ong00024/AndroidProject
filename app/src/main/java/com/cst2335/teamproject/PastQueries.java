2849c481114523c274863d6c5086ef43dc8cd367
Vincent Zheng <zhen0123@algonquinlive.com>
2022-04-03 20:18:17 -0400

    added menu/navigation drawer to activities

--- app/src/main/java/com/cst2335/teamproject/PastQueries.java
+++ app/src/main/java/com/cst2335/teamproject/PastQueries.java
@@ -1,21 +1,33 @@
 package com.cst2335.teamproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -24,10 +36,10 @@ import java.util.ArrayList;
  * Click on one item will bring up details of the queries.
  * Long click on item to prompt delete.
  *
- * @author Kevin Ong
+ * @author Kevin Ong, Vincent Zheng
  */
 
-public class PastQueries extends AppCompatActivity {
+public class PastQueries extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "PastQueries";
 
     CovidOpener myOpener;
@@ -58,6 +70,22 @@ public class PastQueries extends AppCompatActivity {
         myList = findViewById(R.id.theListView);
         theAdaptor = new MyListAdapter();
         myList.setAdapter(theAdaptor);
+
+        //Gets toolbar from the layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.getOverflowIcon().setColorFilter(Color.WHITE,  PorterDuff.Mode.SRC_ATOP);
+
+        //For NavigationDrawer
+        DrawerLayout drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        toggle.getDrawerArrowDrawable().setColorFilter(Color.WHITE,  PorterDuff.Mode.SRC_ATOP);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
 
         //load previously saved db entries
         savedQs = theDB.rawQuery("SELECT * from " + CovidOpener.TABLE_NAME + ";", null);
@@ -141,6 +169,56 @@ public class PastQueries extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String toast = null;
+        //What actions occur when the menu item is selected
+        if (item.getItemId() == R.id.help) {
+            toast = getString(R.string.clickHelp);
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle(R.string.howTo)
+                    .setMessage(getResources().getString(R.string.savedHelp))
+                    .setNeutralButton("OK", (click, b) -> {
+                    })
+                    .create().show();
+        }
+        Toast.makeText(this, toast, Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Intent nextActivity;
+        switch(item.getItemId())
+        {
+            case R.id.itemStart:
+                nextActivity = new Intent (PastQueries.this, MainActivity.class);
+                startActivity(nextActivity);
+                break;
+
+            case R.id.itemSearch:
+                nextActivity = new Intent (PastQueries.this, SearchActivity.class);
+                startActivity(nextActivity);
+                break;
+
+            case R.id.itemSaved:
+                Toast.makeText(this, R.string.onSavedPage, Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return true;
     }
 
     public class MyListAdapter extends BaseAdapter {
--- app/src/main/java/com/cst2335/teamproject/ResultsActivity.java
+++ app/src/main/java/com/cst2335/teamproject/ResultsActivity.java
@@ -1,14 +1,27 @@
 package com.cst2335.teamproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
-import android.content.ContentValues;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -18,7 +31,7 @@ import java.util.ArrayList;
  * @author Vincent Zheng, Kevin Ong
  * @version 1.0
  */
-public class ResultsActivity extends AppCompatActivity{
+public class ResultsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     CovidOpener myOpener;
     SQLiteDatabase theDB;
@@ -35,6 +48,22 @@ public class ResultsActivity extends AppCompatActivity{
         TextView viewResults = findViewById(R.id.tv_result);
         ArrayList<CovidData> results;
         results = (ArrayList<CovidData>) getIntent().getSerializableExtra("Result");
+
+        //Gets toolbar from the layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.getOverflowIcon().setColorFilter(Color.WHITE,  PorterDuff.Mode.SRC_ATOP);
+
+        //For NavigationDrawer
+        DrawerLayout drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        toggle.getDrawerArrowDrawable().setColorFilter(Color.WHITE,  PorterDuff.Mode.SRC_ATOP);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
 
         for (int i = 0; i < results.size() ; i++) {
             sBuild.append(results.get(i).toString());
@@ -62,8 +91,61 @@ public class ResultsActivity extends AppCompatActivity{
             startActivity(saveToPastQueries);
 
 
+
+
         });
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String toast = null;
+        //What actions occur when the menu item is selected
+        if (item.getItemId() == R.id.help) {
+            toast = getString(R.string.clickHelp);
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle(R.string.howTo)
+                    .setMessage(getResources().getString(R.string.resultHelp))
+                    .setNeutralButton("OK", (click, b) -> {
+                    })
+                    .create().show();
+        }
+        Toast.makeText(this, toast, Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Intent nextActivity;
+        switch(item.getItemId())
+        {
+            case R.id.itemStart:
+                nextActivity = new Intent (ResultsActivity.this, MainActivity.class);
+                startActivity(nextActivity);
+                break;
+
+            case R.id.itemSearch:
+                nextActivity = new Intent (ResultsActivity.this, SearchActivity.class);
+                startActivity(nextActivity);
+                break;
+
+
+            case R.id.itemSaved:
+                nextActivity = new Intent (ResultsActivity.this, PastQueries.class);
+                startActivity(nextActivity);
+                break;
+        }
+
+        return true;
+    }
 }
